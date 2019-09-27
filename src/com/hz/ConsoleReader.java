21b6aaae81390ff6dee1a843cf42b843e4095109
Andries Nieuwenhuize <a.nieuwenhuize@hz.nl>
2019-09-27 09:47:04 +0200

    initial

---
+++ src/com/hz/ConsoleReader.java
@@ -0,0 +1,23 @@
+package com.hz;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ConsoleReader {
+
+    private String readLine() {
+
+        String msg;
+
+        try{
+            msg = new BufferedReader(
+                    new InputStreamReader(System.in))
+                    .readLine();
+        }
+        catch(Exception ex) {
+            msg = "Could not read input..";
+        }
+
+        return msg;
+    }
+}
