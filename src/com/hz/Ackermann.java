6e4aab58931de8e7556ff17706e2a56acd199033
Andries Nieuwenhuize <a.nieuwenhuize@hz.nl>
2019-09-27 10:23:47 +0200

    havvy enough

--- src/com/hz/Ackermann.java
+++ src/com/hz/Ackermann.java
@@ -2,6 +2,15 @@ package com.hz;
 
 public class Ackermann {
 
+    public int ccalc(int m, int n) {
+
+        int val = 0;
+        for(int i = 0; i< m*n; i++) {
+            val = this.calculate(m, n);
+        }
+        return val;
+    }
+
     // Ackerman.java
     public int calculate(int m, int n) {
         int result = -1;
--- src/com/hz/RealSubject.java
+++ src/com/hz/RealSubject.java
@@ -7,7 +7,7 @@ public class RealSubject {
         System.out.println("Hello!");
         Ackermann ackermann = new Ackermann();
         // default max: m = 3, n = 11
-        System.out.println(ackermann.calculate(3,11));
+        System.out.println(ackermann.ccalc(2,800));
     }
 
     public String verifyAnswerToTheUniverseAndEveryThing() {
